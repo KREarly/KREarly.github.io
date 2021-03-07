7f3bb5758c8ad3635f920b3483113285572ff734
Use your GitHub username <Enter your email address>
2021-03-06 17:07:30 -0700

    shove 2

--- lesson8/javascript/stormseverity.js
+++ lesson8/javascript/stormseverity.js
@@ -1,3 +1,10 @@
 function adjustRating(rating) {
     document.getElementById("displayLvl").innerHTML = rating;
 }
+
+
+var input = document.getElementById('fullName');
+
+input.oninvalid = function(event) {
+    event.target.setCustomValidity('Full name should only include letters and spaces');
+}
