ad4321fed62092e4a9727fd5dffba3d4d3a3e15e
Use your GitHub username <Enter your email address>
2021-04-08 01:59:04 -0600

    shooooove

---
+++ finalWebsite/js/index.js
@@ -0,0 +1,23 @@
+const requestURL = '../json/stores.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const stores = jsonObject['stores'];
+    for (let i = 0; i < stores.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p = document.createElement('p');
+
+        h2.textContent = stores[i].name;
+        card.appendChild(h2);
+
+        p.innerHTML = "Address:" + stores[i].address + "</br>" + "Phone:" + stores[i].phone + "</br>" + "Website:" + stores[i].website + "</br>" + "E-Mail:" + stores[i].email;
+        card.appendChild(p);
+
+        document.querySelector('div.cards').appendChild(card);
+    }
+  });
